f6896466a7dddc54bd6150ba6cdbd4d54824ce9d
deGuzmanChris <Chris.deguzman8@gmail.com>
2025-05-13 12:44:51 -0600

    created link from root page to week 2 assignment

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
-export default function Page() {
+import Link from "next/link";
+
+export default function MainPage() {
     return (
         <main>
-            <h1>hello world
-            </h1>
-            <p> Link to week 2: <a href= "week-2"> Week 2</a></p>
+            <h1>CPRG 306: Web Development 2 - Assignments</h1>
+            <Link href="/week-2">Week 2 Assignment</Link>
         </main>
     );
 }
--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -3,8 +3,7 @@ import Link from "next/link";
 export default function StudentInfo() {
     return (
         <div>
-            <h1>Student Information</h1>
-            <p>Chris de Guzman</p>
+            <h1>Chris de Guzman</h1>
             <Link href="https://github.com/deGuzmanChris">My Github Account</Link>
         </div>
     );
