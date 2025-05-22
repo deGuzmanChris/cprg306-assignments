de35f1937ed239f20cc62a9fec0092b0ead0f915
deGuzmanChris <Chris.deguzman8@gmail.com>
2025-05-22 11:50:46 -0600

    created all js files for week-3

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function MainPage() {
         <main>
             <h1>CPRG 306: Web Development 2 - Assignments</h1>
             <Link href="/week-2">Week 2 Assignment</Link>
+            <Link href="/week-3">Week 3 As signment</Link>
         </main>
     );
-}
+} 
