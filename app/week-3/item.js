0bc3de1b6d663a8bfe12cbc70a68c3f7918b93fd
deGuzmanChris <Chris.deguzman8@gmail.com>
2025-05-22 12:35:07 -0600

    added the items into a list. and added itemlist component to main page

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,9 +1,13 @@
 export default function Item({name,quantity,category}) {
     return (
         <div>
-            <h2>{name}</h2>
-            <p>Quantity: {quantity}</p>
-            <p>Category: {category}</p>
+            <ul>
+                <li>
+                    <h2>{name}</h2>
+                    <p>Quantity: {quantity}</p>
+                    <p>Category: {category}</p>
+                </li>
+            </ul>
         </div>
     );
 }
